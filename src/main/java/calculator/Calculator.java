fb4b6e0a76f4d886d0a0164b4ba56ac7a21f117d
Tejinder7 <t.saggu99@gmail.com>
2023-03-15 18:32:48 +0530

    first commit

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        while(true){
+            System.out.println("Mini Project - Scientific Calculator with DevOps");
+            System.out.println("Choose Operation");
+
+            System.out.println("1. Square root\n2. Factorial\n3. Natural Logarithm (base e)\n4. Power\n5. Exit\n");
+            return;
+        }
+
+    }
+}
