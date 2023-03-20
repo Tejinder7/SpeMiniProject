72e482aab4d75ceafe82d6cfee240e57b28a7452
Tejinder7 <t.saggu99@gmail.com>
2023-03-20 19:29:04 +0530

    Added JenkinsFile Pipeline Script

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -14,10 +14,17 @@ public class CalculatorTest {
 
     @Test
     public void squareRootTruePositive(){
-        System.out.println("Testing");
-        assertEquals("Finding square root for True Positive", 4, calculator.squareRoot(16), DELTA);
-        assertEquals("Finding square root for True Positive", 1, calculator.squareRoot(1), DELTA);
-        assertEquals("Finding square root for True Positive", 9, calculator.squareRoot(81), DELTA);
-        assertEquals("Finding square root for True Positive", 6, calculator.squareRoot(36), DELTA);
+        assertEquals("Testing square root for True Positive", 5, calculator.squareRoot(25), DELTA);
+        assertEquals("Testing square root for True Positive", 16, calculator.squareRoot(256), DELTA);
+        assertEquals("Testing square root for True Positive", 1, calculator.squareRoot(1), DELTA);
+    }
+
+    @Test
+    public void squareRootFalsePositive(){
+        assertNotEquals("Testing square root for False Positive", 2, calculator.squareRoot(9), DELTA);
+        assertNotEquals("Testing square root for False Positive", 5, calculator.squareRoot(36), DELTA);
+        assertNotEquals("Testing square root for False Positive", 10, calculator.squareRoot(144), DELTA);
+        assertNotEquals("Testing square root for False Positive", 13, calculator.squareRoot(225), DELTA);
+
     }
 }
