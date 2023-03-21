eb065bb682029c6ea31676f00f37d2a1a88eadb7
Tejinder7 <t.saggu99@gmail.com>
2023-03-21 14:25:24 +0530

    Added all functionalities and updated playbook

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,9 +3,6 @@ package calculator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Calculator {
 
     private static final Logger logger = LogManager.getLogger(Calculator.class);
@@ -13,10 +10,43 @@ public class Calculator {
     public Calculator() {
     }
 
-    public double squareRoot(double num1){
-        logger.info("[SQUARE ROOT] - " + num1);
-        double result= Math.sqrt(num1);
+    public double squareRoot(double num){
+        logger.info("[SQUARE ROOT] - " + num);
+        double result= Math.sqrt(num);
         logger.info("[SQUARE ROOT- RESULT] - " + result);
+        return result;
+    }
+
+    public int factorial(int num){
+        logger.info("[FACTORIAL] - "+ num);
+        int result= 1;
+
+        if(num< 0){
+            for(int i= num; i<0; i++){
+                result*= i;
+            }
+        }
+        else{
+            for(int i= 2; i<= num; i++){
+                result*= i;
+            }
+        }
+
+        logger.info("[FACTORIAL - RESULT] - "+ result);
+        return result;
+    }
+
+    public double naturalLog(double num){
+        logger.info("[NATURAL LOGARITHM] - " + num);
+        double result= Math.log(num);
+        logger.info("[NATURAL LOGARITHM- RESULT] - " + result);
+        return result;
+    }
+
+    public double power(double num1, double num2){
+        logger.info("[POWER FUNCTION] - " + num1 +"RAISED TO THE POWER "+ num2);
+        double result= Math.pow(num1, num2);
+        logger.info("[POWER FUNCTION- RESULT] - " + result);
         return result;
     }
 }
--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -10,6 +10,9 @@ public class Main {
         Scanner scanner= new Scanner(System.in);
 
         int choice;
+        double num1;
+        double num2;
+        int factorial;
 
         while(true){
             System.out.println("Mini Project - Scientific Calculator with DevOps");
@@ -30,16 +33,61 @@ public class Main {
                 case 1:
                     System.out.println();
                     System.out.println("Calculate Square Root");
-                    System.out.println("Enter the number:");
-                    double num1= scanner.nextDouble();
+                    System.out.println("Enter the number: ");
+                    num1= scanner.nextDouble();
 
                     System.out.println("Result: "+ calculator.squareRoot(num1));
                     System.out.println();
                     break;
 
-                default:
+                case 2:
+                    System.out.println();
+                    System.out.println("Calculate Factorial of an integer");
+                    System.out.println("Enter the number: ");
+
+                    try{
+                        factorial= scanner.nextInt();
+                    }
+                    catch(InputMismatchException exception){
+                        System.out.println(exception);
+                        System.out.println("Try again and enter a natural number");
+                        return;
+                    }
+
+                    System.out.println("Result: "+ calculator.factorial(factorial));
+                    System.out.println();
+                    break;
+
+                case 3:
+                    System.out.println();
+                    System.out.println("Calculate Natural Logarithm");
+                    System.out.println("Enter the number: ");
+                    num1= scanner.nextDouble();
+
+                    System.out.println("Result: "+ calculator.naturalLog(num1));
+                    System.out.println();
+                    break;
+
+                case 4:
+                    System.out.println();
+                    System.out.println("Calculate Power");
+
+                    System.out.println("Enter the base: ");
+                    num1= scanner.nextDouble();
+                    System.out.println("Enter the exponent: ");
+                    num2= scanner.nextDouble();
+
+                    System.out.println("Result: "+ calculator.power(num1, num2));
+                    System.out.println();
+                    break;
+
+                case 5:
                     System.out.println("Exiting program");
                     return;
+
+                default:
+                    System.out.println("Wrong input. Try Again");
+                    break;
             }
         }
     }
